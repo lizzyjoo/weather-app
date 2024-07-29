580c1765a94da91020373494a2ac542a137f1bf9
lizzyjoo <jejviolinist@gmail.com>
2024-07-29 15:12:56 -0400

    class stores weather info for target hour

---
+++ src/hour.js
@@ -0,0 +1,13 @@
+class Hour{
+    constructor(
+        datetime,
+        temp,
+        precipprob
+    ) {
+        this.datetime = datetime;
+        this.temp = temp;
+        this.precipprob = precipprob;
+    }
+}
+
+export default Hour;
